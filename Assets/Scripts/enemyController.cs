7177dbf9b26227cfb3aa4c7e71d3096f6e33b852
WittlyEli <162608185+EliRod61@users.noreply.github.com>
2025-02-23 03:56:50 -0500

    Graybox testing area, moving platform, enemy AI script

---
+++ Assets/Scripts/enemyController.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class enemyController : MonoBehaviour
+{
+    public NavMeshAgent ai;
+    public List<Transform> destinations;
+    public Animator aiAnim;
+    public float walkSpeed, chaseSpeed, minIdleTime, maxIdleTime, idleTime, sightDistance, catchDistance, chaseTime, minChaseTime, maxChaseTime, jumpscareTime;
+    public bool walking, chasing;
+    public Transform player;
+    Transform currentDest;
+    Vector3 dest;
+    int randNum;
+    public int destinationAmount;
+    public Vector3 rayCastOffset;
+    public string deathScene;
+
+    void Start()
+    {
+        walking = true;
+        randNum = Random.Range(0, destinations.Count);
+        currentDest = destinations[randNum];
+    }
+    void Update()
+    {
+        Vector3 direction = (player.position - transform.position).normalized;
+        RaycastHit hit;
+        //rayCast for detecting the player(its sight)
+        if (Physics.Raycast(transform.position + rayCastOffset, direction, out hit, sightDistance))
+        {
+            //if it sees the player
+            if (hit.collider.gameObject.tag == "Player")
+            {
+                //start chasing and initiate a chase
+                walking = false;
+                StopCoroutine("stayIdle");
+                StopCoroutine("chaseRoutine");
+                StartCoroutine("chaseRoutine");
+                chasing = true;
+            }
+        }
+        //if chasing the player
+        if (chasing == true)
+        {
+            //its destination becomes the player 
+            dest = player.position;
+            ai.destination = dest;
+            ai.speed = chaseSpeed;
+            aiAnim.ResetTrigger("walk");
+            aiAnim.ResetTrigger("idle");
+            aiAnim.SetTrigger("sprint");
+            //distance between player and enemy
+            float distance = Vector3.Distance(player.position, ai.transform.position);
+            //if enemy catches player
+            if (distance <= catchDistance)
+            {
+                //player dies and plays jumpscare then loads selected scene 
+                player.gameObject.SetActive(false);
+                aiAnim.ResetTrigger("walk");
+                aiAnim.ResetTrigger("idle");
+                aiAnim.ResetTrigger("sprint");
+                aiAnim.SetTrigger("jumpscare");
+                StartCoroutine(deathRoutine());
+                chasing = false;
+            }
+        }
+        //if not chasing player
+        if (walking == true)
+        {
+            //Destination becomes the positions set and anims play
+            dest = currentDest.position;
+            ai.destination = dest;
+            ai.speed = walkSpeed;
+            aiAnim.ResetTrigger("sprint");
+            aiAnim.ResetTrigger("idle");
+            aiAnim.SetTrigger("walk");
+            //if reached destination, become idle there then choose next destination randomly
+            if (ai.remainingDistance <= ai.stoppingDistance)
+            {
+                aiAnim.ResetTrigger("sprint");
+                aiAnim.ResetTrigger("walk");
+                aiAnim.SetTrigger("idle");
+                ai.speed = 0;
+                StopCoroutine("stayIdle");
+                StartCoroutine("stayIdle");
+                walking = false;
+            }
+        }
+    }
+    IEnumerator stayIdle()
+    {
+        idleTime = Random.Range(minIdleTime, maxIdleTime);
+        yield return new WaitForSeconds(idleTime);
+        walking = true;
+        randNum = Random.Range(0, destinations.Count);
+        currentDest = destinations[randNum];
+    }
+    IEnumerator chaseRoutine()
+    {
+        chaseTime = Random.Range(minChaseTime, maxChaseTime);
+        yield return new WaitForSeconds(chaseTime);
+        walking = true;
+        chasing = false;
+        randNum = Random.Range(0, destinations.Count);
+        currentDest = destinations[randNum];
+    }
+    IEnumerator deathRoutine()
+    {
+        yield return new WaitForSeconds(jumpscareTime);
+        SceneManager.LoadScene(deathScene);
+    }
+}
