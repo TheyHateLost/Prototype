ab86dced2067e44afca73429d7363763b7eabca0
MichaelR81503 <144148271+MichaelR81503@users.noreply.github.com>
2025-02-26 19:19:39 -0500

    added a pop up UI for Task

---
+++ Assets/Scripts/Events/GameEventsManager.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+public class GameEventsManager : MonoBehaviour
+{
+    public static GameEventsManager instance { get; private set; }
+
+    public InputEvents inputEvents;
+    public PlayerEvents playerEvents;
+    public QuestEvents questEvents;
+    public MiscEvents miscEvents;
+    public GoldEvents goldEvents;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("Found more than one Game Events Manager in the scene.");
+        }
+        instance = this;
+
+        // initialize all events
+        inputEvents = new InputEvents();
+        playerEvents = new PlayerEvents();
+        questEvents = new QuestEvents();
+        goldEvents = new GoldEvents();
+        miscEvents = new MiscEvents();
+    }
+}
