d1c56d60bb04d9def62d2a9c324bbb15b93cb681
WittlyEli <162608185+EliRod61@users.noreply.github.com>
2025-04-21 12:11:52 -0700

    Fixing press and hold task

--- Assets/Scripts/TaskSystem/HoldTasks.cs
+++ Assets/Scripts/TaskSystem/HoldTasks.cs
@@ -3,38 +3,54 @@ using JetBrains.Annotations;
 using UnityEditor.ShortcutManagement;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
-public class HoldTasks : MonoBehaviour
+using UnityEngine.UI;
+public class HoldTasks : MonoBehaviour, IInteractable
 {
-    /*
-    public float 
+    public float holdDuration = 1f;
+    public Image fillCircle;
+
+    float holdTimer;
     bool isHolding = false;
 
+    public static event Action OnHoldComplete;
+
     // Update is called once per frame
+
+    public void Interact()
+    {
+
+    }
     void Update()
     {
-    if (isHolding)
-
-    {
-        holdTimer += Time.deltaTime
-        object Fillcircle = null;
-        int holdDuration = 0;
-        int holdTimer = 0;
-        Fillcircle.fillAmount = holdTimer / holdDuration;
-        if (holdTimer >= holdDuration) 
-        //Hold to complete task
+        if (isHolding)
+        {
+            holdTimer += Time.deltaTime;
+            fillCircle.fillAmount = holdTimer / holdDuration;
+            if (holdTimer >= holdDuration)
+            {
+                //Load next level
+                OnHoldComplete.Invoke();
+                ResetHold();
+            }
+        }
     }
 
     public void onHold(InputAction.CallbackContext context)
-{
-    if (context.started) 
     {
-        isHolding = true;
+        if (context.started)
+        {
+            isHolding = true;
+        }
+        else if (context.canceled)
+        {
+            ResetHold();
+        }
     }
-    else if (context.canceled)
-    {
-        ResetHold();
 
+    void ResetHold()
+    {
+        isHolding = false;
+        holdTimer = 0;
+        fillCircle.fillAmount = 0;
     }
-}*/
 }
